93abbc849302e7c4ff2124d7e8a1155ae16c2a50
xxjose.fernandoxx@gmail.com <xxjose.fernandoxx@gmail.com>
2019-09-28 13:40:23 -0400

    TODO

---
+++ Moron/apiEval/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace apiEval
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiEval.Models.Clown> Clowns { get; set; }
+    }
+}
---
+++ Moron/apiEval/Models/Clown.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace apiEval.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeTricks
+    {
+        Jabalina,
+        Jenga,
+        Jirafa,
+    }
+    public class Clown
+    {
+        [Key]
+        public int ClownID { get; set; }
+        public String Nickname { get; set; }
+        [Required]
+        public TypeTricks Tricks { get; set; }
+        [EmailAddress(ErrorMessage = "")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
+        public String Email { get; set; }
+        [DataType(DataType.Date)]
+        public String Birthdate { get; set; }
+
+    }
+}
